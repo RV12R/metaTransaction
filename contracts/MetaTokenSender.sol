d2635a5f62e1b33bcf07b475a53c490af4ee94d2
Rahul AV <valuhar@gmail.com>
2022-08-25 23:29:30 +0530

    Update MetaTokenSender.sol

--- contracts/MetaTokenSender.sol
+++ contracts/MetaTokenSender.sol
@@ -18,7 +18,9 @@ contract TokenSender {
 
     // Mapping for preventing Signature Replay
     mapping(bytes32 => bool) executed;
-
+    
+    // The message hash is converted to a Ethereum Signed Message Hash according to the EIP-191
+    // Calling this function converts the messageHash into this format "\x19Ethereum Signed Message:\n" + len(message) + message)
     function transfer(address sender, uint amount, address recipient, address tokenContract, uint nonce, bytes memory signature) public {
         bytes32 messageHash = getHash(sender, amount, recipient, tokenContract, nonce);
         bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
